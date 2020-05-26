3c22d68804b0f0378d8845bd96667ad982471b77
Anran Zhang <zhanganran2011@@live.com>
2020-05-26 00:20:10 -0300

    initial commit : Project created

---
+++ A2.c
@@ -0,0 +1,15 @@
+/* File:     A2.c
+ * Author:   Anran Zhang   B00747547
+ * Date:     2020/05/25
+ * Version:  1.0
+ *
+ * Purpose:  This program is a simple C shell.
+ * Notes:
+ *           Command used to compile: gcc A2.c -o A1.out
+ */
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
